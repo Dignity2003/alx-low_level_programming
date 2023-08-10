d2b1b9aab4fb0393b17196569950017c6fc29046
Dignity2003 <austineemeka08cares@gmail.com>
2023-08-10 08:42:49 -0700

    My first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Prints the sentence about programming
- * Return: Always 0 (success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzel");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
