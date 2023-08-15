ccfb655fbf13c56db4dbdd51903e6c26d5663e3e
Dignity2003 <austineemeka08cares@gmail.com>
2023-08-15 20:05:22 +0000

    Prints lowercase alphabet for 10X

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - Prints lowercase alphabet for x10
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+char i;
+int j;
+for (i = 1; i <= 10; i++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+}
+_putchar('\n');
+}
