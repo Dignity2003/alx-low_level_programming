5badb059d734f1c4938dd7e0629286f64203bfde
Dignity2003 <austineemeka08cares@gmail.com>
2023-08-15 19:47:10 +0000

    Prints lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - Prints Lowercase Alphabet
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
