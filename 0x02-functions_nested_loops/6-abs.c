6e96826d4a74c75887efc8eaf50c80ebb1e2b764
Dignity2003 <austineemeka08cares@gmail.com>
2023-08-15 21:28:03 +0000

    Print absolute digit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Prints absolute alphabet
+ * @n: Number to be checked
+ * Return: 
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+return (n)
+}
+else
+return (n);
+}
