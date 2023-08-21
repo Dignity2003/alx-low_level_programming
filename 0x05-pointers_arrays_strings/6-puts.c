d511526f8e3fb90240c15aef1701425a11c48cd6
Dignity2003 <austineemeka08cares@gmail.com>
2023-08-21 20:37:11 +0000

    It prints some part of the string

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts - It prints a string
+ * @str: The string to be printed
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+int i, c = 0;
+for (i = 0; str[i] != '\0'; ++i)
+{
+c++;
+if (c % 2 == 0)
+{
+continue;
+}
+else
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
