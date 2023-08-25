36a5517e4a742ea289c5e29f13a4a444d5e0ddba
Dignity2003 <austineemeka08cares@gmail.com>
2023-08-25 14:34:02 +0000

    It prints reverse of a string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reverse_array - It Prints the reverse of a string
+ * @a: The string that we will print it reverse
+ * @n: The total number of characters in the string
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+for (i = n; a[i] < 0; i--)
+{
+_putchar(a[i]);
+}
+}
