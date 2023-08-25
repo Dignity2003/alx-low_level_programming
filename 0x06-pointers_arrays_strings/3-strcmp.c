056ed62133edfd67d14f57e60d3e0a1e51901436
Dignity2003 <austineemeka08cares@gmail.com>
2023-08-25 14:05:17 +0000

    Compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,21 +14,7 @@ while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] == s2[i])
 {
-return (0);
-}
-else if (s1[i] != s2[i])
-{
-if (s1[i] > s2[i])
-{
 return (s1[i] - s2[i]);
-}
-}
-else if (s1[i] != s2[i])
-{
-if (s2[i] > s1[i])
-{
-return (s2[i] - s1[i]);
-}
 }
 i++;
 }
