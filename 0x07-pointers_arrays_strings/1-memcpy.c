387aca2026945bb9c06b3f1ed6688f4cfd36b7a1
Dignity2003 <austineemeka08cares@gmail.com>
2023-08-29 19:42:24 +0000

    It copies a string to another string

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-for (n = 0; src[n + 1] <= '\0'; n++)
+unsigned int i;
+for (i = 0; i < n ; i++)
 {
-*dest = src[n];
+dest[i] = src[i];
 }
 return (dest);
 }
