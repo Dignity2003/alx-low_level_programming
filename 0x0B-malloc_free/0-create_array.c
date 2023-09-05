4d175ece1ecbb19f24de4a0eb2a02dbcb5e3cd85
Dignity2003 <austineemeka08cares@gmail.com>
2023-09-05 08:56:40 +0000

    Print a particular character in array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * create_array - it set a paticular character to an array
  * @size: The size of the array
@@ -11,15 +11,15 @@ char *create_array(unsigned int size, char c)
 char *a;
 unsigned int i = 0;
 a = malloc((size + 1) * sizeof(char));
-if (a == '\0')
+if (*a == '\0')
 {
 return (0);
 }
 while (i < size)
 {
 a[i] = c;
-i++
+i++;
 }
 a[i] = '\0';
-return (s);
+return (a);
 }
