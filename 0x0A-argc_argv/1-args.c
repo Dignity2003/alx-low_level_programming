4309251196231ded4807988bf4f1c2b748d4dc7d
Dignity2003 <austineemeka08cares@gmail.com>
2023-09-09 22:32:00 +0000

    prints number of argument entered

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * @argc: argument count
+ * @argv: Stores strings
+ * Return: Always 0
+ */
+int main(int argc, char *argv)
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
