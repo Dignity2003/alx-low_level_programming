9f495e0d2291ddddfe8de75907f0acb1caa7f3ab
Dignity2003 <austineemeka08cares@gmail.com>
2023-09-12 22:22:55 +0000

    it copies a struct to another struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * _strlen - Prints the size of string lateral s
+ * @s: The string u want to no the number of character in it
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+;
+return (i);
+}
+/**
+ * _strcpy - It copies a string to another string
+ * @dest: The string u will copy it to
+ * @src: The string u are copying it to
+ * Return: Always 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+int len = 0, i;
+while (src[len] != '\0')
+{
+len++;
+}
+
+for (i = 0; i < len; len++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+return (dest);
+}
+/**
  * new_dog - creates a copy of struct dog
  * @name: The name of the dog
  * @age: The age of the dog
@@ -10,15 +43,30 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *user;
-
-user = malloc(sizeof(dog_t));
-if (user == NULL)
+dog_t *dog;
+int len1, len2;
+len1 = _strlen(name);
+len2 = _strlen(owner);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
 {
 return (NULL);
 }
-user->name = name;
-user->age = age;
-user->owner = owner;
-return (user);
+dog->name = malloc(sizeof(char) * (len1 + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+dog->owner = malloc(sizeof(char) * (len2 + 1));
+if (dog->owner == NULL)
+{
+free(dog);
+free(dog->name);
+return (NULL);
+}
+_strcpy(dog->name, name);
+_strcpy(dog->owner, owner);
+dog->age = age;
+return (dog);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 int _putchar(char c);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
