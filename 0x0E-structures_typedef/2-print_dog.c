d2de32d16fe87ac5117ce358b69ac835d9b3587a
Dignity2003 <austineemeka08cares@gmail.com>
2023-09-12 01:37:54 +0000

    Structures in c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,8 +19,14 @@ else if (d->name == NULL)
 {
 printf("nil\n");
 }
+else if (d->owner == NULL)
+{
+printf("nil\n");
+}
 else
+{
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
+}
 }
