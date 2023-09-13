4740f4c75d8d061e6cd239128bcbedb117e6c780
Dignity2003 <austineemeka08cares@gmail.com>
2023-09-13 23:09:04 +0000

    It prints name using function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_name - it print namme using function pointers
+ * @name: string to add
+ * @f: the function pointer
+ * Return: Always 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || F == NULL)
+{
+return;
+f(name);
+}
