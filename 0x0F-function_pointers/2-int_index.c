1bb3d29d3ec5e0cc497d68558b27f7260cbfdf15
Dignity2003 <austineemeka08cares@gmail.com>
2023-09-13 23:50:01 +0000

    locate a data stored in arrray

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int_index - It searches for an integer in array
+ * @array: the array
+ * @size: size of element
+ * @cmp: it compares a data in array with the seached data
+ * Return: ALWAYS 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array == NULL || size <= 0 || cmp == NULL)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+return (i);
+}
+return (-1);
+}
