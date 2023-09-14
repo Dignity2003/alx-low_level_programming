7bb42e235e6916a743047189deac24e21a160a7b
Dignity2003 <austineemeka08cares@gmail.com>
2023-09-14 20:55:22 +0000

    prints numbers using variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list list;
+unsigned int i, num;
+va_start(list, n);
+for (i = 0; i < n; i++)
+{
+num = va_arg(list, int);
+if (i < n - 1 && separator != NULL)
+{
+printf("%d", num);
+printf("%s", separator);
+}
+else
+printf("%d", num);
+}
+printf("\n");
+}
