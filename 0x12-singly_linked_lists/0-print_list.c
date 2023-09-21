561e47d3d6c88c4d5b90bf8407389e3ed07186a1
Dignity2003 <austineemeka08cares@gmail.com>
2023-09-21 23:45:06 +0000

    it prints characters

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,17 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * print_list - it prints charcter
+ * @h: the head of the node
+ * Return: Total number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 size_t count;
 for (count = 0; h != NULL; count++)
 {
-if (h -> str == NULL)
+if (h->str == NULL)
 {
 printf("[0](nil)");
 }
-printf("[%d] %s\n", h -> len, h -> str);
-h = h -> next;
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
 }
 return (count);
 }
