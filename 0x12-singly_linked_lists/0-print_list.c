b60203b2e1ec6447875917b607dde2b4756ed829
Dignity2003 <austineemeka08cares@gmail.com>
2023-09-22 00:08:03 +0000

    it prints characters

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,19 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t count;
-for (count = 0; h != NULL; count++)
+size_t total_char = 0;
+while (!= NULL)
 {
 if (h->str == NULL)
 {
 printf("[0](nil)");
 }
 else
-printf("[%d] %s\n", h->len, h->str);
+{
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
+total_char++;
 }
-return (count);
+}
+return (total_char);
 }
