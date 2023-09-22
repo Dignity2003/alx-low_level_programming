452cd6f073794a6b9d9b3ce2a6c4742f0069b402
Dignity2003 <austineemeka08cares@gmail.com>
2023-09-22 01:05:18 +0000

    total number of nodes

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * list_len - it returns the total number of elements in a linked list
+ *@h: the pointer the list_t
+ *Return: number of element in h
+ */
+size_t list_len(const list_h *h)
+{
+size_t k = 0;
+while (h)
+{
+n++;
+h = h->next;
+}
+return (k);
+}
